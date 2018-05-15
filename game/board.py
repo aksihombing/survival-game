0997881e2e7a5d993cd50adea557a37654d315b5
aksihombing <31894572+aksihombing@users.noreply.github.com>
2018-05-14 20:25:33 -0400

    Updated board.py
    Created two separated functions for the starting story (story) and first tile (twotwo) and attempted to detect if the input of xchoice and ychoice are not from the given (not sure if it works fully and functionally).

--- game/board.py
+++ game/board.py
@@ -12,19 +12,16 @@ class Game:
   
   
   def __init__(self):
-    self.state = ["[]" for n in range(9)]
+    self.state = [['[]','[]','[]'] for n in range(3)]
+    self.playerplace = self.state[2][2]
     self.game_over = False
-    self.playerplace = self.state[3][3]
-  
   def __repr__(self):
     return "Game({})".format(self.state)
   
   def __str__(self):
     return "{} {} {}\n{} {} {}\n{} {} {}\n".format(*self.state)
   
-  # TEMPORARY CODE (UNTESTED)
-  # The code below is for the start of the game
- def twotwo(self):
+  def story(self):
     print("It's been months since a virus spread. People who were infected died within hours, or at most, a few days, later. People's greed turned nearly everyone against each other.")
     pause()
     print("The group that you were once with has recently turned against you in order to maintain more supplies. Your two good friends decide to join you, staying loyal to you. The group sent the three of you off with minimal supplies to survive about five days, yet no map.")
@@ -36,14 +33,20 @@ class Game:
     friend2 = str(input("What is the name of your second friend?\n"))
     supplies = {"food" : str(randint(10,30)), "water" : str(randint(15,30)), "money" : "${}".format(str(randint(10,30)))}
     print("You have....\n {} food\n {} water\n {} left.".format(*supplies))
+  
+  
+  # TEMPORARY CODE (UNTESTED)
+  # The code below is for the start of the game
+  def twotwo(self):
     while True:
       try:
-        xchoice = int(input("Which way do you want to go? (2,3), (2,2), or (3,2)?\n row:"))
-        if xchoice != 2 or 3:
-          raise ValueError
+        xchoice = int(input("Which way do you want to go? (1,2), (1,1), or (2,1)?\n row:"))
         ychoice = int(input("\n column:"))
-        if ychoice != 2 or 3:
-          raise ValueError
-        break
-      except ValueError:
-        print("Please choose from given.")
+        if int(xchoice) == 2 or 1:
+          if int(ychoice) == 2 or 1:
+            if int(xchoice)+int(ychoice) == 3:
+              return self.playerplace == self.state[int(xchoice)][int(ychoice)]
+        else:
+          raise IndexError
+      except IndexError:
+        print("Please choose from the given")
