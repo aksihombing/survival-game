38db1aa5853648bc2a5f2d503e6f8d2d7e5e6a2e
aksihombing <31894572+aksihombing@users.noreply.github.com>
2018-05-24 22:36:23 -0400

    Created the winning function; Fixed choices for oneone()

--- game/board.py
+++ game/board.py
@@ -15,19 +15,15 @@ class Game:
     self.state = [['[]','[]','[]'] for n in range(3)]
     self.playerplace = self.state[2][2]
     self.game_over = False
-    self.playername = str(input("What is your name?:\n"))
-    self.friend1 = str(input("What is the name of your first friend?\n"))
-    self.friend2 = str(input("What is the name of your second friend?\n"))
-    self.supplies = {"food" : str(randint(10,30)), "water" : str(randint(15,30)), "money" : "${}".format(str(randint(10,30)))}
-    print("You have....\n {} food cans, {} water bottles, {} left.".format(self.supplies['food'],self.supplies['water'],self.supplies['money']))
-    return self.story()
+    self.supplies = {"food" : 32, "water" : 50}
+
 
   def __repr__(self):
     return "Game({})".format(self.state)
   
   def __str__(self):
     return "{} {} {}\n{} {} {}\n{} {} {}\n".format(*self.state)
-  
+
   def story(self):
     print("It's been months since a virus spread. People who were infected died within hours, or at most, a few days, later. People's greed turned nearly everyone against each other.")
     pause()
@@ -38,24 +34,24 @@ class Game:
     return self.twotwo()
   
   
-  # TEMPORARY CODE (UNTESTED)
-  # The code below is for the start of the game
+  # NOT YET "DEBUGGED" ----------------------------
+
   def twotwo(self):
     while True:
       try:
-        xchoice = int(input("Which way do you want to go? (1,2), (1,1), or (2,1)?\n row:"))
-        ychoice = int(input("\n column:"))
-        if int(xchoice) == 2:
-          if int(ychoice) == 1:
-            return self.twoone()
-        elif int(xchoice) == 1:
-          if int(ychoice) == 1:
-            return self.oneone()
-          if int(ychoice) == 2:
-            return self.onetwo()
+        firstchoice = int(input("Which way do you wish to go?\n 1.)North: a city\n 2.)East: a river\n 3.)Northeast: nothing but trees!\n"))
+        if int(firstchoice) == 1:
+          pause()
+          return self.twoone()
+        elif int(firstchoice) == 2:
+          pause()
+          return self.onetwo()
+        elif int(firstchoice) == 3:
+          pause()
+          return self.oneone()
         else:
           raise IndexError
-      except IndexError:
+      except:
         print("Please choose from the given")
   
   def twoone(self):
@@ -64,37 +60,81 @@ class Game:
         print("There is a large city ahead of your group. Do you wish to continue?\n Enter 1 to continue or 2 to travel east instead\n")
         continuechoice = int(input())
         if int(continuechoice) == int(1):
+          pause()
           print("Your group follows you deep into the city.")
           pause()
           print("Oh no! The city is infested, and unfortunately your group has been slowed down and infected.")
           pause()
-          print("'Help...' you hear {} groan. You and {} hear {}'s groaning when it's too late!").format(self.friend1, self.friend2, self.friend1)
+          print("'Help...' you hear one friend groan. You and your other friend hears their groaning when it's too late!")
           pause()
-          print("{} grabbed their shoulders and shook them, in disbelief of a lost friend. {}'s head looked up and before you knew it, {} was also infected.").format(self.friend2, self.friend1, self.friend2)
+          print("Your other friend grabbed their shoulders and shook them, in disbelief of a lost friend. Your sick friend's head looked up and before you knew it, your other friend was also infected.")
           pause()
-          print("'{}! Run far away! Please!' {} shouts.").format(self.playername, self.friend2)
+          print("Run far away! Please!' your conscious, sick friend shouts.")
           pause()
           print("Sucks to be you, huh? It gets worse, too. In an attempt to flee, you hadn't been paying attention to where you ran. You turn around and see a horde of infected people chasing after you, the only healthy person around.")
           pause()
           print("G A M E  O V E R")
           self.game_over == True
         elif int(continuechoice) == 2:
+          pause()
           return self.oneone()
+          
       except:
-        return
+        return "Error. Please try again."
  
  
   def oneone(self):
     while True:
       try:
-        print("'There seems to be many ways to go from here!' {} cheerfully says.").format(self.friend2)
+        print("'There seems to be many ways to go from here!' your friend cheerfully says.")
         pause()
-        print("{} goes on and on and ON, describing the surroundings, other than the boring trees.").format(self.friend2)
+        print("They go on and on and ON, describing the surroundings, other than the boring trees.")
         pause()
         print("Cutting you some slack, here's what they said, in a simpler and easy way.\n NORTHWEST - A strange building\n NORTH - A huge river\n NORTHEAST - A small town\n EAST - A city\n SOUTH - A small and thin river\n SOUTHWEST - There seems to be a light somewhere behind the trees\n WEST - Another small town.")
+        pause()
+        print("Where do you want to go?")
+        direction = int(input("1.)Northwest\n 2.)North\n 3.)Northeast\n 4.)East\n 5.)South\n 6.)Southwest\n 7.)West\n"))
+        if int(direction) == 1 :
+          pause()
+          return self.zerozero()
+        elif int(direction) == 2 :
+          pause()
+          return self.onezero()
+        elif int(direction) == 3 :
+          pause()
+          return self.twozero()
+        elif int(direction) == 4 :
+          pause()
+          return self.twoone()
+        elif int(direction) == 5 :
+          pause()
+          return self.onetwo()
+        elif int(direction) == 6 :
+          pause()
+          return self.zerotwo()
+        elif int(direction) == 7 :
+          pause()
+          return self.zeroone()
+        else:
+          raise ValueError
         
       except:
-        return "ok"
+        return "Error. Please try again."
+
+  def onezero(self):
+    pass
+  
+  def twozero(self):
+    pass
+  
+  def zerotwo(self):
+    pass
+  
+  def zeroone(self):
+    pass
+
+
+
   def onetwo(self):
     while True:
       try:
@@ -104,9 +144,31 @@ class Game:
           print("The water is really quick! Your group holds hands and tries to cross together.")
           pause()
           print("After some more shouting, you and your friends finally make it across, but at the cost of some supplies.")
-          return self.playerplace[0][2], supplies[water]-4, supplies[food]-6, supplies[money]-10
+          pause()
+ 
         elif int(crossornot) == int(2):
           print("Your group heads north instead")
+          pause()
           return self.playerplace[1][1]
       except:
-        return "ok"
+        return "Error. Please try again."
+
+
+
+
+
+
+  # ------------- W I N ----------------
+  def zerozero(self):
+    print("After countless hours of losing sleep, having to ration whatever was left of your supplies, you have finally reached a less horrifying building.")
+    pause()
+    print("'Hey! Are you survivors? Are any of you infected?' someone shouts at the top of the wall in front of you. You and your friends nod very quickly.")
+    pause()
+    print("The man at the top seems to just stand and stare at you all. After about a few seconds, he says....")
+    pause()
+    print("'Well, come on in!'")
+    pause()
+    print("The gates slowly open and the three of you ran with all your energy to get inside.")
+    pause()
+    print("You have successfully arrived at the base!\n CONGRATULATIONS!\n YOU WIN")
+    return self.game_over == True
